f1eb1b8dd7b663963fb3c36faa3c479fbf5130b7
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2024-02-06 15:43:32 -0500

    Add starter code and task list

---
+++ script.js
@@ -0,0 +1,7 @@
+// Declare your arrays here
+
+// Make sure to declare your HTML elements as variables! 
+
+// Submit Button 
+let submitButton = document.querySelector("button");
+submitButton.addEventListener("click", function() {});
