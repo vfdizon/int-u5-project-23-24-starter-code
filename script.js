4fa58f63be0e145d806ad48ee10ca378b7ca833d
Vincent Dizon <vdizon@cps.edu>
2024-04-29 15:26:40 +0000

    base functionality

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
 // Declare your arrays here
 
 // Make sure to declare your HTML elements as variables! 
+let genreMap = {
+    "action": [
+        "Die Hard",
+        "The Dark Knight",
+        "Mad Max: Fury Road"
+    ],
+    "comedy": [
+        "Superbad",
+        "The Hangover",
+        "Anchorman: The Legend of Ron Burgundy"
+    ],
+    "drama": [
+        "The Shawshank Redemption",
+        "Pulp Fiction",
+        "Forrest Gump"
+    ],
+};
+
+
+
 
 // Submit Button 
-let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
+let submitButton = document.querySelector(".submit");
+submitButton.addEventListener("click", function () {
+    let userInput = document.querySelector(".input").value;
+    userInput = userInput.toLowerCase().trim();
+
+    let movieDiv = document.querySelector(".movies");
+    movieDiv.innerHTML = "";
+    let movieArray = genreMap[userInput];
+    movieArray.forEach(name => {
+        movieDiv.appendChild(getElement(name));
+    });
+});
+
+function getElement(movieName) {
+    let newElement = document.createElement("div");
+
+    let title = document.createElement("p");
+    let text = document.createTextNode(movieName);
+    title.append(text);
+    newElement.append(title);
+
+    
+    return newElement;
+}
